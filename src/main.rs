05bdf569ec4609f08d9463353f8e3c5a585fc54f
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-08-31 14:44:07 +1000

    chore: Ask the question

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,7 @@ fn main() {
     println!("Hello, world!");
     println!("Hello, world!");
     println!("Hello, world!");
+
+    let question = "What is the answer to life, the universe, and everything?".to_string();
+
 }
