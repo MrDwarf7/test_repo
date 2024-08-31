c1fab229f8d9b8fb06563daaf950af5f51366af0
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-08-31 14:56:40 +1000

    chore: remove a section of code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 fn main() {
     println!("Hello, world!");
-
-    println!("Hello, world!");
-    println!("Hello, world!");
-    println!("Hello, world!");
     println!("Hello, world!");
 
     let question = "What is the answer to life, the universe, and everything?".to_string();
