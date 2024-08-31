e140f34c08fc739a52e606c59d7953d37f14ece6
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-08-31 14:44:27 +1000

    feat(question): As the burning question

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,11 @@ fn main() {
 
     let question = "What is the answer to life, the universe, and everything?".to_string();
 
+    if the_answer(question) == 42 {
+        println!("Correct!");
+    } else {
+        println!("Incorrect!");
+    }
+}
+
 }
